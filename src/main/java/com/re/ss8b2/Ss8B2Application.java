5a3716aab41c7440dc04e12ecf40325a4c793138
LLL <Zeikezan12345@gmail.com>
2026-05-22 08:07:10 +0700

    B2

--- src/main/java/com/re/ss8b2/Ss8B2Application.java
+++ src/main/java/com/re/ss8b2/Ss8B2Application.java
@@ -2,8 +2,10 @@ package com.re.ss8b2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class Ss8B2Application {
 
     public static void main(String[] args) {
